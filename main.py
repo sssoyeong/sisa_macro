c5a5568c3cff40d3ff90df4ffffd54f15abab6e6
sssoyeong <sypark.for.work@gmail.com>
2023-11-16 21:50:32 +0900

    231116 ~ing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 현황 체크
 
 나
-300 -> 362
+300 -> 375
 그사람
 250 -> 289
 """
@@ -13,8 +13,8 @@ import pandas as pd
 list09 = pd.read_csv('history/course_list_231109.csv', index_col=0)
 list16 = pd.read_csv('course_list_231116.csv', index_col=0)
 
-list09_fin = pd.DataFrame([list09.loc[idx] for idx in list09.index if list09['수료여부'][idx]=='TRUE'])
-list16_fin = pd.DataFrame([list16.loc[idx] for idx in list16.index if list16['수료여부'][idx]=='TRUE'])
+list09_fin = pd.DataFrame([list09.loc[idx] for idx in list09.index if (list09['수료여부'][idx]=='TRUE')|(list09['수료여부'][idx]=='True')])
+list16_fin = pd.DataFrame([list16.loc[idx] for idx in list16.index if (list16['수료여부'][idx]=='TRUE')|(list16['수료여부'][idx]=='True')])
 print(list09_fin.shape, list16_fin.shape)
 
 
@@ -22,8 +22,33 @@ list09_fin_real = pd.DataFrame([list09_fin.loc[idx] for idx in list09_fin.index 
 list16_fin_real = pd.DataFrame([list16_fin.loc[idx] for idx in list16_fin.index if list16_fin['소분류'][idx]!='생활교양자격증'])
 
 print(list09_fin_real.shape, list16_fin_real.shape)
-
 print(list16_fin.shape[0]-list09_fin.shape[0])
 print(list16_fin_real.shape[0]-list09_fin_real.shape[0])
 
 
+"""
+나: 생활교양자격증 넘긴 거 빼면 63개 수료, 보수적으로 60개 수료했다고 가정.
+그: 39개 수료
+"""
+hers = list09_fin.loc[311:394]  # 40개
+mine = list16_fin.tail(60)      # 60개
+
+hour_hers = sum(hers['학습시간'])
+hour_mine = sum(mine['학습시간'])
+print('7일간 수료한 강의들 학습시간 합계', hour_hers, hour_mine)                #  499  / 1003
+print('7일간 실질적으로 학습한 시간 합계', hour_hers*0.3, hour_mine*0.3)        # 149.7 / 300.9
+print('7일간 실질적으로 쏟은 시간 하루당', hour_hers*0.3/7, hour_mine*0.3/7)    # 21.39 / 42.99
+
+
+# 다시 계산해볼까?
+course_list = list09.copy()
+# course_list.loc[289:330]
+sum(course_list.loc[289:330]['학습시간'])*0.3/7     # 14.53 hours
+# 그 사람이 14.53시간 쏟아부었다고? 아니, 하루 평균 7.26시간 부었다고 하는 게 훨씬 적절함. 적어도 두 개로 돌리고 있다는 말이 됨
+
+
+
+##############################
+# 2023.10.18 ~ 2023.11.30 (44일)
+# 44 * 24 = 1056 hours
+# 1056 / 0.3 = 3520 hours 수료 가능
